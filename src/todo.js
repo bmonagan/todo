83cc2449d58cafb92b8ed667371e4b93650ea5bf
bmonagan <bernardmonagan@gmail.com>
2026-03-17 12:01:25 -0400

    Add Todo class for managing task details and completion status

---
+++ src/todo.js
@@ -0,0 +1,16 @@
+class Todo {
+    constructor(title, description, dueDate, priority, notes) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.finished = false;
+    }
+    FinishTodo() {
+        this.finished = true;
+    }
+
+}
+
+    
