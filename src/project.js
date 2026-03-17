4f2ea34ea283be9d195fc011c68fe009fcdada09
bmonagan <bernardmonagan@gmail.com>
2026-03-17 15:48:14 -0400

    Refactor Project class constructor to initialize todoList with an array check

--- src/project.js
+++ src/project.js
@@ -1,16 +1,16 @@
 export class Project {
-    constructor(name, priority, todoList) {
-        this.name = name;
-        this.priority = priority;
-        this.todoList = [];
-    }
-    changeName(newName) {
-        this.name = newName;
-    }
-    changePriority(newPriority) {
-        this.priority = newPriority;
-    }
-    addTodo(todo) {
-        this.todoList.push(todo);
-    }
+	constructor(name, priority, todoList = []) {
+		this.name = name;
+		this.priority = priority;
+		this.todoList = Array.isArray(todoList) ? todoList : [];
+	}
+	changeName(newName) {
+		this.name = newName;
+	}
+	changePriority(newPriority) {
+		this.priority = newPriority;
+	}
+	addTodo(todo) {
+		this.todoList.push(todo);
+	}
 }
